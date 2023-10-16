de0daf089b09ca94ca51661adbf133e543506937
Alix Lamouroux <lamouroux.alix@yahoo.fr>
2023-10-16 18:23:53 +0200

    fix

--- fmristroke/workflows/bold/base.py
+++ fmristroke/workflows/bold/base.py
@@ -3,7 +3,7 @@ Orchestrating the BOLD-preprocessing workflow
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 .. autofunction:: init_lesion_preproc_wf
-.. autofunction:: init_func_derivatives_wf
+.. autofunction:: init_func_lesion_derivatives_wf
 
 """
 import os
