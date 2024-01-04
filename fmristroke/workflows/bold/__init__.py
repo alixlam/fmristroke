090b8c332f4381ec25f8481b48755a6374646bf1
Alix LAMOUROUX <a19lamou@sl-mee-br-115.imta.fr>
2024-01-04 10:38:15 +0100

    Update doc

--- fmristroke/workflows/bold/__init__.py
+++ fmristroke/workflows/bold/__init__.py
@@ -9,6 +9,7 @@ Lesion preprocessing fMRI - BOLD signal workflows
 .. automodule:: fmristroke.workflows.bold.registration
 .. automodule:: fmristroke.workflows.bold.denoise
 .. automodule:: fmristroke.workflows.bold.concatenate
+.. automodule:: fmristroke.workflows.bold.connectivity
 
 
 """
@@ -19,6 +20,7 @@ from .registration import init_lesionplot_wf
 from .lagmaps import init_hemodynamic_wf
 from .denoise import init_denoise_wf
 from .concatenate import init_concat_wf
+from .connectivity import init_connectivity_wf
 
 __all__ = [
     'init_lesion_preproc_wf',
@@ -27,4 +29,5 @@ __all__ = [
     'init_hemodynamic_wf',
     'init_denoise_wf',
     'init_concat_wf',
+    'init_connectivity_wf'
 ]
